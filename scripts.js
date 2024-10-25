2204b77aed50bb39534cd15dea60728142d45b32
kdorado471 <136772776+kdorado471@users.noreply.github.com>
2024-10-24 21:03:29 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,16 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('contactForm');
+
+
+    form.addEventListener('submit', function(event) {
+        
+        if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        
+        form.classList.add('was-validated');
+    }, false);
+});
